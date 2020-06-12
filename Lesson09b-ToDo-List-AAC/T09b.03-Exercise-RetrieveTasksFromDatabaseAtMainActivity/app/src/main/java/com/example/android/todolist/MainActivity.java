c7fdc6f9ebaa11118e60402c523e8da6bb5db9f0
El-Safty <afawzy629@gmail.com>
2020-06-12 20:01:27 +0200

    Solve T09b.03-Exercise-RetrieveTasksFromDatabaseAtMainActivity

--- Lesson09b-ToDo-List-AAC/T09b.03-Exercise-RetrieveTasksFromDatabaseAtMainActivity/app/src/main/java/com/example/android/todolist/MainActivity.java
+++ Lesson09b-ToDo-List-AAC/T09b.03-Exercise-RetrieveTasksFromDatabaseAtMainActivity/app/src/main/java/com/example/android/todolist/MainActivity.java
@@ -1,18 +1,18 @@
 /*
-* Copyright (C) 2016 The Android Open Source Project
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*      http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package com.example.android.todolist;
 
@@ -26,6 +26,8 @@ import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.helper.ItemTouchHelper;
 import android.view.View;
 
+import com.example.android.todolist.database.AppDatabase;
+
 import static android.support.v7.widget.DividerItemDecoration.VERTICAL;
 
 
@@ -33,11 +35,10 @@ public class MainActivity extends AppCompatActivity implements TaskAdapter.ItemC
 
     // Constant for logging
     private static final String TAG = MainActivity.class.getSimpleName();
+    AppDatabase mDb;
     // Member variables for the adapter and RecyclerView
     private RecyclerView mRecyclerView;
     private TaskAdapter mAdapter;
-
-    // TODO (1) Create AppDatabase member variable for the Database
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -92,7 +93,7 @@ public class MainActivity extends AppCompatActivity implements TaskAdapter.ItemC
             }
         });
 
-        // TODO (2) Initialize member variable for the data base
+        mDb=AppDatabase.getInstance(this);
     }
 
     /**
@@ -103,7 +104,7 @@ public class MainActivity extends AppCompatActivity implements TaskAdapter.ItemC
     @Override
     protected void onResume() {
         super.onResume();
-        // TODO (3) Call the adapter's setTasks method using the result
+        mAdapter.setTasks(mDb.taskDao().loadAllTasks());
         // of the loadAllTasks method from the taskDao
     }
 
