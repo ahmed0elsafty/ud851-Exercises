7585074917cd2eb6df172c7fd5081f5e51544615
El-Safty <afawzy629@gmail.com>
2020-06-29 08:01:49 +0200

    Solve T11.03-Exercise-LandscapeLayout

--- Lesson11-Completeing-The-UI/T11.02-Exercise-DataBinding/app/src/main/java/com/example/android/boardingpass/MainActivity.java
+++ Lesson11-Completeing-The-UI/T11.02-Exercise-DataBinding/app/src/main/java/com/example/android/boardingpass/MainActivity.java
@@ -1,23 +1,22 @@
 package com.example.android.boardingpass;
 
 /*
-* Copyright (C) 2016 The Android Open Source Project
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*      http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 import android.databinding.DataBindingUtil;
-import android.icu.text.DateTimePatternGenerator;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
@@ -27,7 +26,6 @@ import com.example.android.boardingpass.utilities.FakeDataUtils;
 import java.text.SimpleDateFormat;
 import java.util.Locale;
 import java.util.concurrent.TimeUnit;
-import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -39,7 +37,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        mBinding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
         BoardingPassInfo fakePassData = FakeDataUtils.generateFakeBoardingPassInfo();
 
@@ -67,8 +65,8 @@ public class MainActivity extends AppCompatActivity {
 
         long totalMinutes = info.getMinutesUntilBoarding();
         long hours = TimeUnit.MINUTES.toHours(totalMinutes);
-        long minutesLessHourse=totalMinutes-TimeUnit.HOURS.toMinutes(hours);
-        String hourseAndMinutes =getString(R.string.countDownFormat,hours,minutesLessHourse);
+        long minutesLessHourse = totalMinutes - TimeUnit.HOURS.toMinutes(hours);
+        String hourseAndMinutes = getString(R.string.countDownFormat, hours, minutesLessHourse);
         mBinding.textViewBoardingInCountdown.setText(hourseAndMinutes);
     }
 }
